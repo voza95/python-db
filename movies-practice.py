2ae4cad25451c39ede1e5e35544569678dd9095f
Vaibhav Oza <35027648+voza95@users.noreply.github.com>
2020-12-27 21:46:16 +0530

    first commit

---
+++ movies-practice.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+connection = sqlite3.connect('movies.db')
+
+cursor = connection.cursor()
+
+# cursor.execute("INSERT INTO Movies VALUES ('Taxi Driver', 'Martin Scorsese', 1976)")
+
+# cursor.execute("SELECT * FROM Movies")
+
+
+# famousFilms = [('Pulp Fiction','Quentin Tarantino', 1994),
+# ('Back to the Future', 'Steven Spielberg', 1985),
+# ('Moonrise Kingdom', 'Wes Anderson', 2012)]
+
+# cursor.executemany('Insert INTO Movies VALUES (?,?,?)', famousFilms)
+
+# records = cursor.execute("SELECT * FROM Movies")
+# print(cursor.fetchone())
+# print(cursor.fetchall())
+
+# for record in records:
+    # print(record)
+
+release_year = (1985, )
+cursor.execute("SELECT * FROM Movies WHERE year=?", release_year)
+print(cursor.fetchall())
+
+connection.commit()
+connection.close()
